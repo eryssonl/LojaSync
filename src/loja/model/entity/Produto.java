92f96a33761f0f195090f3814b256a5de933ad9a
erysson <eryssonplay11@gmail.com>
2026-04-24 15:36:52 -0300

    feat: nova funcionalidade fifo

--- src/loja/model/entity/Produto.java
+++ src/loja/model/entity/Produto.java
@@ -1,47 +1,81 @@
 package loja.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Produto {
 
-    //private String idProduto;
+    //private String idProduto; FUTURAMENTE
+
     private String nome;
     private double preco;
-    private double custo;
-    private int quantidade;
+    private List <Lote> lotes = new ArrayList<>();
     private int estoqueMinimo = 2;
 
-    public Produto(String nome, double preco, double custo, int quantidade) {
+    public Produto() {}
+    public Produto(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
-        this.quantidade = quantidade;
-        this.custo = custo;
-        //this.idProduto = java.util.UUID.randomUUID().toString();
+
     }
 
-    //public String getIdProduto() { return idProduto; }
-    public String getNome() {
-        return nome;
+    // =====================
+    //         FIFO
+    // =====================
+    public void adicionarLote(double custo, int quantidade) {
+        lotes.add(new Lote(custo, quantidade));
     }
-    public int getQuantidade() {
-        return quantidade;
-    }
-    public double getPreco() { return preco; }
-    public double getCusto() { return custo; }
-    public int getEstoqueMinimo() { return estoqueMinimo; }
 
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
+    // =====================
+    //   GETTERS DERIVADOS
+    // =====================
+    public int getQuantidadeTotal(){
+        return lotes.stream().mapToInt(Lote::getQuantidade).sum();
     }
-    public void setEstoqueMinimo(int estoqueMinimo) {this.estoqueMinimo = estoqueMinimo;}
+
+    public double getCustoMedio() {
+        int total = getQuantidadeTotal();
+        if (total == 0) return 0;
+
+        double soma = lotes.stream()
+                .mapToDouble(l -> l.getCusto() * l.getQuantidade())
+                .sum();
+
+        return soma / total;
+    }
 
     public double getMargemLucro(){
-        return preco - custo; //lucro unitario
+        return preco - getCustoMedio();
     }
     public double getMargemPercentual(){
+        double custo = getCustoMedio();
         if(custo == 0) return 0;
         return ((preco - custo ) / custo) * 100;
     }
     public double getLucroTotal(){
-        return (preco - custo) * quantidade;
+        return (preco - getCustoMedio()) * getQuantidadeTotal();
     }
 
+    // =====================
+    //  GETTERS and SETTERS
+    // =====================
+    public String getNome() {
+        return nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public int getEstoqueMinimo() {
+        return estoqueMinimo;
+    }
+    public List <Lote> getLotes() {
+        return lotes;
+    }
+
+    public void setEstoqueMinimo(int estoqueMinimo) {
+        this.estoqueMinimo = estoqueMinimo;
+    }
+    public void setLotes(List <Lote> lotes) {
+        this.lotes = lotes;
+    }
 }
